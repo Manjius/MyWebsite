03c582c1bdd6ceab837033fdbcd57002ee7a00c3
Manjius <jon.irigoyen.c@gmail.com>
2026-03-04 17:06:18 +0100

    Animate header with path reveal using bushes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 window.onload = () => {
+  const heroHeader = document.querySelector('.hero-header');
+  if (heroHeader) {
+    heroHeader.classList.add('reveal');
+  }
+
   document.querySelectorAll('.side-img').forEach((image) => {
     image.style.transform = 'translateX(0)';
   });
